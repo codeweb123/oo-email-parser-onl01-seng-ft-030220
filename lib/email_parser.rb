826c5ee8fe7da94db2025137828175ac428a8471
interactive-sequence-2417 <jiyonj@hotmail.com>
2020-03-15 06:00:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,13 @@
 class EmailAddressParser
   attr_accessor :emails
-  
+
   def initialize(emails)
     @emails = emails
   end
-  
+
   def parse
     puts splitEmail = emails.split.collect {|address| address.split(',')}
     returnArr = splitEmail.flatten.uniq
     returnArr
   end
 end
-
